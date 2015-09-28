7c1ff56d8c849ab4fc4086087bb2b57e93844786
Kevin Mulhern <kevin.mulhern@onthebeach.co.uk>
2015-09-28 12:00:36 +0100

    Work in progress

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,16 +1,25 @@
+
 class Bottles
-	class Bottles
     def verse(num)
-        if num > 2
-            "#{num} bottles of beer on the wall, #{num} bottles of beer.\nTake one down and pass it around, #{num-1} bottles of beer on the wall.\n"
-    elsif num == 2
-      "#{num} bottles of beer on the wall, #{num} bottles of beer.\nTake one down and pass it around, #{num-1} bottle of beer on the wall.\n"
-    elsif num == 1
-      "#{num} bottle of beer on the wall, #{num} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    else
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    	next_num = num -1
+     	if num >= 1
+            "#{bottles_left(num)} bottle#{plural_ending(num)} of beer on the wall, #{num} bottle#{plural_ending(num)} of beer.\nTake #{prenoun(num)} down and pass it around, #{bottles_left(next_num)} bottle#{plural_ending(next_num)} of beer on the wall.\n"
+    	else
+      		"#{bottles_left(num)} bottle#{plural_ending(num)} of beer on the wall, #{bottles_left(num)} bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    	end
     end
+
+    def prenoun(num)
+    	num == 1 ? "it" : "one"
     end
+
+    def bottles_left(num)
+    	num > 0 ? num : 'no more'
+    end
+
+    def plural_ending(number)
+		number != 1 ? "s" : ""
+ 	end
 
   def verses(num, num2)
     result = ""
@@ -24,4 +33,29 @@ class Bottles
     verses(99,0)
   end
 end
-end
+
+
+
+
+# class Bottles
+
+# 	def verse(verse_number)
+# 		bottle_verse(verse_number)
+# 	end
+
+# 	def bottle_verse(bottle_number)
+# 		"#{bottle_number} bottle#{plural_ending(bottle_number)} of beer on the wall, #{bottle_number} bottle#{plural_ending(bottle_number)} of beer.\n#{bottle_verse_ending(bottle_number)}"
+# 	end
+
+# 	def plural_ending(number)
+# 		number > 1 ? "s" : ""
+# 	end
+
+# 	def bottle_verse_ending(bottle_number)
+# 		if bottle_number > 1
+# 			"Take one down and pass it around, #{bottle_number-1} bottle#{plural_ending(bottle_number -1)} of beer on the wall.\n"
+# 		else
+# 			"Take it down and pass it around, no more bottles of beer on the wall.\n"
+# 		end
+# 	end
+# end
