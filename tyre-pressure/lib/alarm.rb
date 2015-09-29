daecae721e4fc7b7303d2830c5a8d1e609c8ede3
Kevin Mulhern <kevin.mulhern@onthebeach.co.uk>
2015-09-29 11:01:53 +0100

    configured alarm class and first tests

--- tyre-pressure/lib/alarm.rb
+++ tyre-pressure/lib/alarm.rb
@@ -1,3 +1,18 @@
 class Alarm
-  # Fill in the implementation here
+  
+  def initialize(sensor)
+  	@sensor = sensor
+  	@on = false
+  end
+
+  def check
+  	pressure = @sensor.sample_pressure
+  	if pressure < 17.5 || pressure > 21
+  		@on = true
+  	end
+  end
+
+  def on?
+  	@on
+  end
 end
--- tyre-pressure/spec/alarm_spec.rb
+++ tyre-pressure/spec/alarm_spec.rb
@@ -1,4 +1,45 @@
 require 'alarm'
+require 'sensor'
 RSpec.describe "tyre pressure alarm" do
-  it "needs some tests"
+	subject { Alarm.new(sensor) }
+	let(:sensor) { double("sensor", :sample_pressure => 17.5) }
+
+	before do
+		subject.check
+	end
+	
+  it "initialize's alarm class with sensor value" do
+  	subject
+  end
+
+  it "makes sure sensor can call sample pressure" do
+  	expect( sensor ).to receive(:sample_pressure)
+  	subject.check
+  end
+
+  context "low tyre pressure" do
+  	let(:sensor) { double("sensor", :sample_pressure => 17) }
+
+  	it "checks low tyre pressure" do 
+  		expect( subject.on? ).to eq(true)
+  	end
+  end
+
+  context "high tyre pressure" do
+    let(:sensor) { double("sensor", :sample_pressure => 22) }
+
+    it "checks high tyre pressure" do
+      expect( subject.on? ).to eq(true)
+    end
+  end
+
+  context "tyre pressure within range" do
+    let(:sensor) { double("sensor", :sample_pressure => 18) }
+
+    it "checks false response for alarm when pressure in range" do 
+      expect( subject.on?).to eq(false)
+    end
+  end
+
+
 end
